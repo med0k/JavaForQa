9dd330a1a468dff524babf004e64f27031b78696
Max Zvoleiko <zvoleikom@gmail.com>
2018-11-27 00:13:43 +0200

    Merge branch 'master' of https://github.com/med0k/JavaForQa
    # Conflicts:
    #	LR_3/src/Main.java

--- LR_3/src/Main.java
+++ LR_3/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test Git changes!!New changes");
+        System.out.println("Max commits");
     }
 }
