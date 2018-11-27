9d1915996d3367c58a6ca7158b02b8f1f71f3064
Kateryna Gulyayevska <31535347+KaterynaGulyayevska@users.noreply.github.com>
2018-11-27 23:54:25 +0200

    Update Main.java
    test merge

--- LR_3/src/Main.java
+++ LR_3/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Max commits");
         System.out.println("Test Git changes!!New changes by MAx");
-        System.out.println("Kate made chnages through IDEA / lets check merge");
+        System.out.println("Kate made chnages through GIT / lets check merge");
     }
 }
