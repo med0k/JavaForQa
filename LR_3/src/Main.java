22ecc39b249ec6fc10efb0db51cc377500c91a79
kateg <k.gulyayevska@gmail.com>
2018-11-28 19:59:41 +0200

    Merge branch 'master' of https://github.com/med0k/JavaForQa
    # Conflicts:
    #	LR_3/src/Main.java

--- LR_3/src/Main.java
+++ LR_3/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Max commits");
-        System.out.println("Test Git changes!!New changes by MAx");
-        System.out.println("Some new changes from Kat_GIt");
+        System.out.println("Test Git changes by MAx");
+        System.out.println("Some new changes by Kat");
     }
 }
