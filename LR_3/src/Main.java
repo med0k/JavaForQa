ce279836e2e036519961b66710281ed4b743b322
Max Zvoleiko <zvoleikom@gmail.com>
2018-11-29 11:36:18 +0200

    Changes by Max

--- LR_3/src/Main.java
+++ LR_3/src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Max commits");
         System.out.println("Test Git changes!!New changes by MAx");
+        System.out.println("Did somebody do home work?");
+
+        // Common guys. Add your commits here.
     }
 }
