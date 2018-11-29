9866916650e655bbc59ec943ca9c054a8e9dc263
Max Zvoleiko <zvoleikom@gmail.com>
2018-11-29 17:54:39 +0200

    sout from Maximus

--- LR_3/src/Main.java
+++ LR_3/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Max commits");
+        System.out.println("Here start Max commit");
         System.out.println("Test Git changes!!New changes by Great Maximus!!!!");
         System.out.println("Did somebody do home work?");
 
